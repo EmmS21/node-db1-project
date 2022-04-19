040da98163725050fe3096a189465a23e16ab826
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2022-04-19 15:17:20 -0500

    completed

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-
 const server = express();
-
 server.use(express.json());
-
+const accountRouter = require('./accounts/accounts-router')
+server.use('/api/accounts',accountRouter)
 module.exports = server;
